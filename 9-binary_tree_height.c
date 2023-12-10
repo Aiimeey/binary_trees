09ecef071f2800c067c70ea73cb7d414c7ebd0f2
imane <imanezenasnii@gmail.com>
2023-12-10 14:52:07 +0100

    Apply a betty correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,6 +6,7 @@
  *
  * Return: size_t representing height, 0 on failure or NULL
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_subtree_size = 0;
@@ -14,7 +15,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l_subtree_size += (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
-	r_subtree_size += (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
+	if (tree->left)
+	{
+		l_subtree_size = 1 + binary_tree_height(tree->left);
+	}
+
+	if (tree->right)
+	{
+		r_subtree_size = 1 + binary_tree_height(tree->right);
+	}
+
 	return ((l_subtree_size > r_subtree_size) ? l_subtree_size : r_subtree_size);
 }
