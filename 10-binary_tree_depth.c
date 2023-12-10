e540d891e884e3e35de799fadda2ace96fd0bfef
imane <imanezenasnii@gmail.com>
2023-12-10 23:06:00 +0100

    Apply betty correction

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,6 +13,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	depth += (tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0;
+	if (tree->parent)
+		depth = 1 + binary_tree_depth(tree->parent);
+
 	return (depth);
+
 }
